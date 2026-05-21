1ee576e657ce4b34f6a89b25ca4a00f4f61e2702
zhopiich <zhoping9449@gmail.com>
2026-05-21 20:19:10 +0800

    feat(router): map root route to HomeView component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
-import { h } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
+
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,9 +8,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: {
-        render: () => h('main'),
-      },
+      component: HomeView,
     },
   ],
 })
